8b1aca17530d2289f140d22f1ee78acf695a8688
Hieu <ngo089120@gmail.com>
2025-03-27 12:53:07 +0700

    queue.c and sched.c

--- src/sched.c
+++ src/sched.c
@@ -15,23 +15,26 @@ static struct queue_t mlq_ready_queue[MAX_PRIO];
 static int slot[MAX_PRIO];
 #endif
 
-int queue_empty(void) {
+int queue_empty(void)
+{
 #ifdef MLQ_SCHED
 	unsigned long prio;
 	for (prio = 0; prio < MAX_PRIO; prio++)
-		if(!empty(&mlq_ready_queue[prio])) 
+		if (!empty(&mlq_ready_queue[prio]))
 			return -1;
 #endif
 	return (empty(&ready_queue) && empty(&run_queue));
 }
 
-void init_scheduler(void) {
+void init_scheduler(void)
+{
 #ifdef MLQ_SCHED
-    int i ;
+	int i;
 
-	for (i = 0; i < MAX_PRIO; i ++) {
+	for (i = 0; i < MAX_PRIO; i++)
+	{
 		mlq_ready_queue[i].size = 0;
-		slot[i] = MAX_PRIO - i; 
+		slot[i] = MAX_PRIO - i;
 	}
 #endif
 	ready_queue.size = 0;
@@ -40,68 +43,105 @@ void init_scheduler(void) {
 }
 
 #ifdef MLQ_SCHED
-/* 
+/*
  *  Stateful design for routine calling
  *  based on the priority and our MLQ policy
  *  We implement stateful here using transition technique
  *  State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
  */
-struct pcb_t * get_mlq_proc(void) {
-	struct pcb_t * proc = NULL;
+struct pcb_t *get_mlq_proc(void)
+{
+	struct pcb_t *proc = NULL;
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
-	return proc;	
+	pthread_mutex_lock(&queue_lock);
+	int flag = 0;
+	for (int i = 0; i < MAX_PRIO; i++)
+	{
+		// not have slot in cpu
+		if (slot[i] == 0)
+		{
+			slot[i] = MAX_PRIO - i;
+			flag++;
+			continue;
+		}
+
+		// queue is empty
+		if (empty(&mlq_ready_queue[i]))
+		{
+			++flag;
+			// all queue is empty
+			if (flag == MAX_PRIO)
+				break;
+			continue;
+		}
+
+		flag = 0;
+		proc = dequeue(&mlq_ready_queue[i]);
+		--slot[i];
+		break;
+	}
+
+	pthread_mutex_unlock(&queue_lock);
+
+	return proc;
 }
 
-void put_mlq_proc(struct pcb_t * proc) {
+void put_mlq_proc(struct pcb_t *proc)
+{
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&mlq_ready_queue[proc->prio], proc);
 	pthread_mutex_unlock(&queue_lock);
 }
 
-void add_mlq_proc(struct pcb_t * proc) {
+void add_mlq_proc(struct pcb_t *proc)
+{
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&mlq_ready_queue[proc->prio], proc);
-	pthread_mutex_unlock(&queue_lock);	
+	pthread_mutex_unlock(&queue_lock);
 }
 
-struct pcb_t * get_proc(void) {
+struct pcb_t *get_proc(void)
+{
 	return get_mlq_proc();
 }
 
-void put_proc(struct pcb_t * proc) {
+void put_proc(struct pcb_t *proc)
+{
 	proc->ready_queue = &ready_queue;
 	proc->mlq_ready_queue = mlq_ready_queue;
-	proc->running_list = & running_list;
+	proc->running_list = &running_list;
 
 	/* TODO: put running proc to running_list */
-
 
 	return put_mlq_proc(proc);
 }
 
-void add_proc(struct pcb_t * proc) {
+void add_proc(struct pcb_t *proc)
+{
 	proc->ready_queue = &ready_queue;
 	proc->mlq_ready_queue = mlq_ready_queue;
-	proc->running_list = & running_list;
+	proc->running_list = &running_list;
 
 	/* TODO: put running proc to running_list */
 
 	return add_mlq_proc(proc);
 }
 #else
-struct pcb_t * get_proc(void) {
-	struct pcb_t * proc = NULL;
+struct pcb_t *get_proc(void)
+{
+	struct pcb_t *proc = NULL;
 	/*TODO: get a process from [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
 	return proc;
 }
 
-void put_proc(struct pcb_t * proc) {
+void put_proc(struct pcb_t *proc)
+{
 	proc->ready_queue = &ready_queue;
-	proc->running_list = & running_list;
+	proc->running_list = &running_list;
 
 	/* TODO: put running proc to running_list */
 
@@ -110,16 +150,15 @@ void put_proc(struct pcb_t * proc) {
 	pthread_mutex_unlock(&queue_lock);
 }
 
-void add_proc(struct pcb_t * proc) {
+void add_proc(struct pcb_t *proc)
+{
 	proc->ready_queue = &ready_queue;
-	proc->running_list = & running_list;
+	proc->running_list = &running_list;
 
 	/* TODO: put running proc to running_list */
 
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);	
+	pthread_mutex_unlock(&queue_lock);
 }
 #endif
-
-
